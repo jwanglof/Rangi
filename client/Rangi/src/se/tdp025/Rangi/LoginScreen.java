050c1dad02f66fa0aab85075b30db6296177b954
Henrik Forsberg <henrik.se.forsberg@gmail.com>
2012-10-31 11:07:45 +0100

    Fixed a few tiny errors

--- client/Rangi/src/se/tdp025/Rangi/LoginScreen.java
+++ client/Rangi/src/se/tdp025/Rangi/LoginScreen.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import com.example.Rangi.R;
 
 public class LoginScreen extends Activity {
     public void onCreate(Bundle savedInstanceState) {
--- client/Rangi/src/se/tdp025/Rangi/MainMenu.java
+++ client/Rangi/src/se/tdp025/Rangi/MainMenu.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-import com.example.Rangi.R;
 
 public class MainMenu extends Activity {
     public void onCreate(Bundle savedInstanceState) {

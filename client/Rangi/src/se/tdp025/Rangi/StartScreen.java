3dea37535e05d85ab059263eaaeb748e5630dac8
Johan <jwanglof@gmail.com>
2012-12-05 14:32:36 +0100

    Added register, login and changed which screen to show after splash

---
+++ client/Rangi/src/se/tdp025/Rangi/StartScreen.java
@@ -0,0 +1,33 @@
+package se.tdp025.Rangi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Johan
+ * Date: 2012-12-05
+ * Time: 14:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class StartScreen extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startscreen);
+    }
+
+    public void login(View view) {
+        Intent login = new Intent(this, LoginScreen.class);
+        startActivity(login);
+    }
+
+    public void register(View view) {
+        //Intent register = new Intent(this, RegisterScreen.class);
+        //startActivity(register);
+
+        Intent register = new Intent(this, MainMenu.class);
+        startActivity(register);
+    }
+}

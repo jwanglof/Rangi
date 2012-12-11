8f88820b4cd58e23162bc0d88b6133ddee8cdd69
Johan Wanglof <jwanglof@gmail.com>
2012-12-11 15:09:16 +0100

    Removed some unused import statements

--- client/Rangi/src/se/tdp025/Rangi/RegisterScreen.java
+++ client/Rangi/src/se/tdp025/Rangi/RegisterScreen.java
@@ -1,13 +1,11 @@
 package se.tdp025.Rangi;
 
 import android.app.Activity;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -37,7 +35,6 @@ public class RegisterScreen extends Activity {
     /**
      * Called when the activity is first created.
      */
-    //@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registerscreen);
